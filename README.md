fdf68045bbda441f175a961c44db3ce5c41204f9
mk-pmb <mk-pmb@users.noreply.github.com>
2016-10-10 14:39:19 +0200

    0.161010.0

---
+++ README.md
@@ -0,0 +1,24 @@
+﻿
+<!--#echo json="package.json" key="name" underline="=" -->
+typechecks-pmb
+==============
+<!--/#echo -->
+
+<!--#echo json="package.json" key="description" -->
+Yet another collection of typechecks.
+<!--/#echo -->
+
+API
+---
+
+:TODO:
+
+
+<!--#toc stop="scan" -->
+
+
+License
+-------
+<!--#echo json="package.json" key=".license" -->
+ISC
+<!--/#echo -->
---
+++ typechecks.js
@@ -0,0 +1,47 @@
+﻿/*jslint indent: 2, maxlen: 80, continue: false, unparam: false, node: true */
+/* -*- tab-width: 2 -*- */
+'use strict';
+
+module.exports = (function setup() {
+  var is = {
+    ary: Array.isArray,
+    fin: Number.isFinite,
+    fun: function (x) { return ((typeof x) === 'function'); },
+    num: function (x) { return ((typeof x) === 'number'); },
+    obj: function (x) { return ((x && typeof x) === 'object'); },
+    str: function (x) { return ((typeof x) === 'string'); },
+  };
+
+  is.clsnm = function (obj, cmp) {
+    var os = Object.prototype.toString.call(obj);
+    if (cmp) { return (os === ('[object ' + cmp + ']')); }
+    return os.replace(/^\[object /, '').replace(/\]$/, '');
+  };
+
+  /*jslint eqeq:true */
+  is.weaklyEqual = function (a, b) { return (a == b); };
+  /*jslint eqeq:false */
+  is.strictlyEqual = function (a, b) { return (a === b); };
+
+  is.ifFunc = function (f, d, t) { return (is.fun(f) ? (t || f) : d); };
+
+  is.undef = function (x, then) {
+    if (x === undefined) {
+      if (arguments.length > 1) { return then; }
+      return true;
+    }
+    if (arguments.length > 1) { return x; }
+    return false;
+  };
+
+  is.numInRange = function (x, a, b) {
+    return ((x === +x) && (x >= a) && (x <= b));
+  };
+
+
+
+
+
+
+  return is;
+}());
