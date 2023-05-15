de8b2c8d2c256e771c9fb1b2569d0c4509388191
mk-pmb <mk-pmb@users.noreply.github.com>
2023-05-15 05:23:45 +0200

    Add simple tests for in2.

--- test/simple.js
+++ test/simple.js
@@ -14,6 +14,10 @@ eq(false, is.same(NaN, 'NaN'));
 eq(false, is.same({}, {}));
 eq(false, is.same([], []));
 
+eq(true,  is.in2(1, 1, 3));
+eq(true,  is.in2(1, 3, 1));
+eq(false, is.in2(1, 2, 3));
+eq(false, is.in2(1, 3, 2));
 
 
 
