023284c2aa39546d4a10d3469f55b17155a93ae0
mk-pmb <mk-pmb@users.noreply.github.com>
2024-02-29 22:02:11 +0100

    v0.231212.2: Better preview for arrays.

--- src/lazyrepr.js
+++ src/lazyrepr.js
@@ -4,33 +4,44 @@
 
 
 function lazyRepr(x) {
-  var t = typeof x, s = lazyRepr.val(t, x);
-  if (s === t) { return t; }
-  return t + ' ' + s;
-}
-
-
-lazyRepr.val = function lazyReprVal(t, x) {
-  var s;
+  var t = typeof x, s;
   try {
     s = String(x);
   } catch (e) {
     s = '[cannot stringify: ' + e + ']';
   }
+  if (x && Array.isArray(x)) { return lazyRepr.ary(x, s); }
+  s = lazyRepr.val(t, x, s);
+  if (s === t) { return t; }
+  return t + ' ' + s;
+}
+
+
+lazyRepr.val = function lazyReprVal(t, x, s) {
   if (t === s) { return s; } // null, undefined
-  if (t === 'object') {
-    if (s === '[object Object]') {
-      s = Object.keys(x).join(', ');
-      return '{' + (s && ' ') + s + (s && ' ') + '}';
-    }
-    return s;
-  }
-  if (t === 'function') {
-    return s.split(/\s*(\{|\))/).slice(0, 2).join('');
-  }
+  if (t === 'object') { return lazyRepr.obj(x, s); }
+  if (t === 'function') { return s.split(/\s*(\{|\))/).slice(0, 2).join(''); }
   if (t === 'string') { return JSON.stringify(s); }
   return s;
 };
+
+
+lazyRepr.obj = function lazyReprObj(x, s) {
+  if (!x) { return s; }
+  if (s === '[object Object]') {
+    s = Object.keys(x).join(', ');
+    return '{' + (s && ' ') + s + (s && ' ') + '}';
+  }
+  return s;
+};
+
+
+lazyRepr.ary = function lazyReprObj(x, s) {
+  var p = s.slice(0, 64);
+  if (p !== s) { p += '…'; }
+  return 'array<' + x.length + '> [' + p + ']';
+};
+
 
 
 
